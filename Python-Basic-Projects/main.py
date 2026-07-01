79edef3fcf383cc168c9b6cbe59f38163ee3d937
omkarkinage-codes <omkarkinage74@gmail.com>
2026-07-01 20:43:51 +0530

    Delete Python-Basic-Projects/main.py

--- Python-Basic-Projects/main.py
+++
@@ -1,8 +0,0 @@
-# Importing the custom module with a short alias 'm'
-import mymodule as m
-
-# Calling the greet function
-m.greet("Omkar")
-
-# Calling and printing the power result 
-print("The power is:-", m.calculate_power(2, 3))
